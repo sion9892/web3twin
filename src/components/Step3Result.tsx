1e642180fc7942df707552413ed82dd10ac100f7
sion9892 <sion9892@users.noreply.github.com>
2025-11-09 02:41:53 +0900

    fix: transactionHash prop 추가

--- src/components/Step3Result.tsx
+++ src/components/Step3Result.tsx
@@ -508,6 +508,7 @@ export default function Step3Result({
           address={address}
           result={result}
           userInfo={userInfo}
+          transactionHash={hash}
         />
 
       </div>
